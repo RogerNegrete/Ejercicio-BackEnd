997d29d5781a315a7b6607d846e2976a5d2dba38
Roger Negrete <rnegretec@est.ups.edu.ec>
2025-01-27 10:25:39 -0500

    segundo commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+// Ruta principal con más texto
 app.get('/', (req, res) => {
-  res.send('¡Hola, Mundo!');
+  res.send('¡Hola, Mundo! Este es un servidor creado con Node.js y Express. ¡Bienvenido a nuestra aplicación!');
 });
 
+// Iniciamos el servidor
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
